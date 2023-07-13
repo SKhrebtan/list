c4e292a6b3107f7deb48e2adae13978876ec70d2
SKhrebtan <shrebtan@gmail.com>
2023-07-13 22:22:07 +0300

    add dogs

--- src/weather.js
+++ src/weather.js
@@ -4,7 +4,7 @@ import debounce from 'lodash.debounce';
 const weatherBlock = document.querySelector('.weather-section');
 const weatherInput = document.querySelector('.weather-input');
 
-weatherInput.addEventListener('input', debounce(onInputType, 3000   ));
+weatherInput.addEventListener('input', debounce(onInputType, 3000));
 let city = 'kiev';
 
 function onInputType(e) {
@@ -16,11 +16,17 @@ function onInputType(e) {
 
 function fetchByName(city) {
         return fetch(`http://api.weatherstack.com/current?access_key=6c248080c44a632b775f09a3c36d8942&query=${city}`)
-        .then(response => { return response.json() })
+            .then(response => {
+                if (response.status !== 200) {
+                    throw new Error(response.status);
+                }
+                return response.json()
+            })
     .catch(error => {console.log(error)})
 }
     
 function onWeatherRender(city) {
+    console.log(city);
         const markup = `
         <ul class="weather-list">
         <li class="weather-list-item"><span class="weather-list">Temperature: </span>${city.current.temperature}</li>
@@ -29,4 +35,4 @@ function onWeatherRender(city) {
         <li class="weather-list-item"><span class="weather-list">Lat: </span>${city.location.lat}</li>
         </ul>`;
         weatherBlock.innerHTML = markup;
-}
+};
